0c96624d396a1f08cf77ecb5f2d726d18faad768
ponomaREG <42519654+ponomaREG@users.noreply.github.com>
2024-05-22 22:05:51 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,11 @@
-// swift-tools-version:5.6
+// swift-tools-version: 5.6
+
+import PackageDescription
 
 let package = Package(
     name: "Ios Test",
     products: [
-        .executable(name: "ios", targets: ["Ios"]),
+        .executable(name: "ios", targets: ["ios"]),
     ],
     targets: [
       .executableTarget(name: "ios")
